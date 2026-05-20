ba464a3109a76e57ac418b33b60b168cc5de2334
Anyarat Namwong <anyarat.na@yipintsoi.com>
2026-05-20 13:46:24 +0700

    ADD

--- src/app/pages/register-success/register-success.ts
+++ src/app/pages/register-success/register-success.ts
@@ -1,12 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-register-success',
-  imports: [RouterLink],
+  imports: [],
   templateUrl: './register-success.html',
   styleUrl: './register-success.css',
 })
-export class RegisterSuccess {
-  
-}
+export class RegisterSuccess {}
